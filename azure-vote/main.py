013f4baf72a822141ff11a7899ee81085c3ca502
ValentinBumeder <valentinbumeder@gmail.com>
2024-06-24 13:53:37 +0100

    Fixed custom_event tracing

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -9,6 +9,7 @@ from datetime import datetime
 
 # App Insights
 from opencensus.ext.azure.log_exporter import AzureLogHandler
+from opencensus.ext.azure.log_exporter import AzureEventHandler
 from opencensus.ext.azure import metrics_exporter
 from opencensus.stats import aggregation as aggregation_module
 from opencensus.stats import measure as measure_module
@@ -18,28 +19,38 @@ from opencensus.tags import tag_map as tag_map_module
 from opencensus.ext.azure.trace_exporter import AzureExporter
 from opencensus.trace.samplers import ProbabilitySampler
 from opencensus.trace.tracer import Tracer
+from opencensus.trace import config_integration
 from opencensus.ext.flask.flask_middleware import FlaskMiddleware
 
-# Logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__) 
+stats = stats_module.stats
+view_manager = stats.view_manager
 
+# Opencensus integration
+config_integration.trace_integrations(['logging'])
+config_integration.trace_integrations(['requests'])
+
+# Logging
+logger = logging.getLogger(__name__) 
+handler = AzureLogHandler(connection_string='InstrumentationKey=6807e2e3-203c-45de-93e3-62dc850dd1fe')
+handler.setFormatter(logging.Formatter('%(traceId)s %(spanId)s %(message)s'))
+logger.addHandler(handler)
+
+# Logging custom events
+logger.addHandler(AzureEventHandler(connection_string='InstrumentationKey=6807e2e3-203c-45de-93e3-62dc850dd1fe'))
+
+# Set the logging level
 logger.setLevel(logging.INFO)
-logger.addHandler(AzureLogHandler(
-    connection_string='InstrumentationKey=6807e2e3-203c-45de-93e3-62dc850dd1fe;IngestionEndpoint=https://westus-0.in.applicationinsights.azure.com/;LiveEndpoint=https://westus.livediagnostics.monitor.azure.com/;ApplicationId=e37674a2-eb61-4fb4-baff-74b8291756b4')
-)
 
 # Metrics
-exporter = metrics_exporter.new_metrics_exporter(
-    enable_standard_metrics=True,
-    connection_string='InstrumentationKey=6807e2e3-203c-45de-93e3-62dc850dd1fe;IngestionEndpoint=https://westus-0.in.applicationinsights.azure.com/;LiveEndpoint=https://westus.livediagnostics.monitor.azure.com/;ApplicationId=e37674a2-eb61-4fb4-baff-74b8291756b4'
-)
+exporter =metrics_exporter.new_metrics_exporter(
+    enable_standard_metrics=True, 
+    connection_string='InstrumentationKey=6807e2e3-203c-45de-93e3-62dc850dd1fe')
+view_manager.register_exporter(exporter)
 
 # Tracing
 tracer = Tracer(
     exporter=AzureExporter(
-        connection_string='InstrumentationKey=6807e2e3-203c-45de-93e3-62dc850dd1fe;IngestionEndpoint=https://westus-0.in.applicationinsights.azure.com/;LiveEndpoint=https://westus.livediagnostics.monitor.azure.com/;ApplicationId=e37674a2-eb61-4fb4-baff-74b8291756b4'
-    ),
+        connection_string = 'InstrumentationKey=6807e2e3-203c-45de-93e3-62dc850dd1fe'),
     sampler=ProbabilitySampler(1.0),
 )
 
@@ -48,8 +59,8 @@ app = Flask(__name__)
 # Requests
 middleware = FlaskMiddleware(
     app,
-    exporter=AzureExporter(connection_string='InstrumentationKey=6807e2e3-203c-45de-93e3-62dc850dd1fe;IngestionEndpoint=https://westus-0.in.applicationinsights.azure.com/;LiveEndpoint=https://westus.livediagnostics.monitor.azure.com/;ApplicationId=e37674a2-eb61-4fb4-baff-74b8291756b4'),
-    sampler=ProbabilitySampler(rate=1.0),
+    exporter=AzureExporter(connection_string='InstrumentationKey=6807e2e3-203c-45de-93e3-62dc850dd1fe'),
+    sampler=ProbabilitySampler(1.0),
 )
 
 # Load configurations from environment or config file
@@ -83,28 +94,39 @@ if not r.get(button2): r.set(button2,0)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-
-    if request.method == 'GET':
-
+    if request.method == 'GET':        
         # Get current values
         vote1 = r.get(button1).decode('utf-8')
-        with tracer.span(name='Cats Vote'):
-            pass
-
-        with tracer.span(name='Dogs Vote'):
-            vote2 = r.get(button2).decode('utf-8')
-            pass
-
+        with tracer.span(name="Cats Vote") as span: 
+            print("Cats Vote")
+        
+        vote2 = r.get(button2).decode('utf-8')
+        with tracer.span("Dogs Vote") as span: 
+            print("Dogs Vote")
+        
         # Return index with values
         return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
-
     elif request.method == 'POST':
-
         if request.form['vote'] == 'reset':
 
             # Empty table and return results
             r.set(button1,0)
             r.set(button2,0)
+
+            vote1 = r.get(button1).decode('utf-8') 
+            properties = {'custom_dimensions': {'Cats Vote': vote1}}
+            logger.info('Cats Vote', extra=properties)
+
+            vote2 = r.get(button2).decode('utf-8')
+            properties = {'custom_dimensions': {'Dogs Vote': vote2}}
+            logger.info('Dogs Vote', extra=properties)
+            
+            return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
+        else:
+            # Insert vote result into DB
+            vote = request.form['vote']
+            r.incr(vote,1)
+            # Get current values
             vote1 = r.get(button1).decode('utf-8')
             properties = {'custom_dimensions': {'Cats Vote': vote1}}
             logger.info('Cats Vote', extra=properties)
@@ -114,20 +136,6 @@ def index():
             logger.info('Dogs Vote', extra=properties)
 
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
-
-        else:
-
-            # Insert vote result into DB
-            vote = request.form['vote']
-            r.incr(vote,1)
-
-            # Get current values
-            vote1 = r.get(button1).decode('utf-8')
-            vote2 = r.get(button2).decode('utf-8')
-
-            # Return results
-            return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
-
 if __name__ == "__main__":
     # TODO: Use the statement below when running locally
     app.run() 
